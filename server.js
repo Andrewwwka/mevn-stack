ac70c5c69f172d252ba2fc34a664bcba3995cccd
Andrew <andrewalthoff@outlook.com>
2025-10-18 19:44:09 -0500

    initil commit, installation of node.js and npm

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the MEVN Stack Application!');  
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log('Backend server is up and running!');
+});
