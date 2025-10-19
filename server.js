945f3cb444d10ee02bca9a48d637bc7eac768943
Andrew <andrewalthoff@outlook.com>
2025-10-18 19:52:08 -0500

    mongoDB and mongoose added as new dependencies with all their files

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB Connected: ${conn.connection.host}');
+    } catch (error) {
+        console.error('Error: ${error.message}');
+        process.exit(1);
+    }
+}
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Welcome to the MEVN Stack Application!');  
